c5a1283944ed2b7694ed8751b5471ebeb122d5ce
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-04-21 15:58:48 +0300

    Изменил название базы для анти скама. Просто более эстетично

--- Structures/Schemas/AntiScamDB.js
+++ Structures/Schemas/AntiScamDB.js
@@ -1,6 +1,6 @@
 const { model, Schema } = require("mongoose");
 
-module.exports = model("AntiScamDB", new Schema({
+module.exports = model("Anti-Scam", new Schema({
     GuildID: String,
     ChannelID: String
 }));
