9e6b8c8c62c54103a491c82824e08e5912ab8cec
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-04-22 18:05:26 +0300

    Думаю, надо бы переработать логгер. Пока, просто переименую файл в Latest.log

--- Utilites/Logger.js
+++ Utilites/Logger.js
@@ -6,7 +6,7 @@
 
 const fs = require("fs");
 const logFilePath = "./Structures/Logs";
-const logFileName = "Latest";
+const logFileName = "Latest.log";
 
 //===========================================================//
 
