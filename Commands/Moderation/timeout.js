0885e9d517a044543acfdf1715a74d0b30e73bc6
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-04-29 02:29:10 +0300

    Максимально, минимальные изменения

--- Commands/Moderation/timeout.js
+++ Commands/Moderation/timeout.js
@@ -7,13 +7,13 @@ module.exports = {
     permission: "MANAGE_MESSAGES",
     options: [
         { name: "mute", description: "Замьютить пользователя", type: "SUB_COMMAND", options: [
-            { name: "user", description: "Укажите пользователя для мьюта.", type: "USER", required: true },
-            { name: "length", description: "Укажите время мьюта [ 1s, 1m, 1h, 1d, 28d ]", type: "STRING", required: true },
-            { name: "reason", description: "Укажите причину мьюта", type: "STRING", required: false }]
+            { name: "user", description: "Укажите пользователя", type: "USER", required: true },
+            { name: "length", description: "Укажите время [ 1s, 1m, 1h, 1d, 28d ]", type: "STRING", required: true },
+            { name: "reason", description: "Укажите причину", type: "STRING", required: false }]
         },
         { name: "unmute", description: "Размьютить пользователя", type: "SUB_COMMAND", options: [
-            { name: "user", description: "Укажите пользователя для размьюта", type: "USER", required: true },
-            { name: "reason", description: "Укажите причину размьюта", type: "STRING", required: false }]
+            { name: "user", description: "Укажите пользователя", type: "USER", required: true },
+            { name: "reason", description: "Укажите причину", type: "STRING", required: false }]
         }
     ],
     /**
@@ -21,24 +21,24 @@ module.exports = {
      */
      async execute(interaction) {
         const { options, member } = interaction;
-        const target = options.getMember("user");
+        const Target = options.getMember("user");
         const length = options.getString("length");
-        const reason = options.getString("reason") || "Причина не указана";
+        const Reason = options.getString("reason") || "Причина не указана!";
         const maxtime = ms("28d");
         if(length) timeInMs = ms(length);
 
-        if(target.user.bot)
+        if(Target.user.bot)
         return interaction.reply({content: 'Нельзя замьютить бота...', ephemeral: true});
         
-        if(target.permissions.has("ADMINISTRATOR"))
-        return interaction.reply({content: `Пользователь ${target.user} является администратором.`, ephemeral: true});
+        if(Target.permissions.has("ADMINISTRATOR"))
+        return interaction.reply({content: `Пользователь ${Target.user} является администратором.`, ephemeral: true});
 
-        if(target.roles.highest.position >= member.roles.highest.position)
+        if(Target.roles.highest.position >= member.roles.highest.position)
         return interaction.reply({content: 'Нельзя мьютить пользователя с более высокой ролью.', ephemeral: true});
 
         switch (options.getSubcommand()) {
             case "mute": {
-                if(target.id === member.id) 
+                if(Target.id === member.id) 
                 return interaction.reply({content: "Эй... Нельзя замьютить самого себя", ephemeral: true});
 
                 if(!timeInMs)
@@ -47,26 +47,26 @@ module.exports = {
                 if(timeInMs > maxtime )
                 return interaction.reply({content: "Пожалуйста, укажите время от 1 секунды до 28 дней!", ephemeral: true});
 
-                if(reason.length > 512)
+                if(Reason.length > 512)
                 return interaction.reply({content: "Причина не может быть больше 512 символов", ephemeral: true});
 
-                target.timeout(timeInMs, reason);
+                Target.timeout(timeInMs, Reason);
                 return interaction.reply({embeds: [new MessageEmbed().setColor("GREEN").setTitle(`Успешный мьют!`)
-                    .setThumbnail(target.displayAvatarURL({dynamic: true, size: 512})).addFields(
-                    {name: "Пользователь:", value: `\`\`\`${target.user.username}\`\`\``, inline: true},
+                    .setThumbnail(Target.displayAvatarURL({dynamic: true, size: 512})).addFields(
+                    {name: "Пользователь:", value: `\`\`\`${Target.user.username}\`\`\``, inline: true},
                     {name: "Время:", value: `\`\`\`${length}\`\`\``, inline: true},
-                    {name: "Причина:", value: `\`\`\`${reason}\`\`\``})], ephemeral: true
+                    {name: "Причина:", value: `\`\`\`${Reason}\`\`\``})], ephemeral: true
                 });
             }
             case "unmute": {
-                if(!target.communicationDisabledUntilTimestamp)
-                return interaction.reply({content: `${target.user.username} не замьючен!`, ephemeral: true});
+                if(!Target.communicationDisabledUntilTimestamp)
+                return interaction.reply({content: `${Target.user.username} не замьючен!`, ephemeral: true});
 
-                await target.timeout(null);
+                await Target.timeout(null);
                 return interaction.reply({embeds: [new MessageEmbed().setColor("GREEN").setTitle("Успешный размьют!")
-                    .setThumbnail(target.displayAvatarURL({dynamic: true, size: 512}))
-                    .addFields({name: "Пользователь:", value: `\`\`\`${target.user.username}\`\`\``},
-                    {name: "Причина:", value: `\`\`\`${reason}\`\`\``})], ephemeral: true
+                    .setThumbnail(Target.displayAvatarURL({dynamic: true, size: 512}))
+                    .addFields({name: "Пользователь:", value: `\`\`\`${Target.user.username}\`\`\``},
+                    {name: "Причина:", value: `\`\`\`${Reason}\`\`\``})], ephemeral: true
                 });
             }
         }
