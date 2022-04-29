33f4d45782d7dc4a26ce3d2147aa4b93c4464104
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-04-29 20:18:00 +0300

    Добавил /kick. Очень похожа на /ban, только не банит)

---
+++ Commands/Moderation/kick.js
@@ -0,0 +1,54 @@
+const { CommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "kick",
+    description: "Выгнать (кикнуть)",
+    permission: "KICK_MEMBERS",
+    options: [
+        { name: "target", description: "Укажите пользователя", type: "USER", required: true },
+        { name: "reason", description: "Укажите причину", type: "STRING", required: false }
+    ],
+    /**
+     * @param {CommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const Target = interaction.options.getMember("target");
+        const Reason = interaction.options.getString("reason") || "Причина не указана!";
+        await Target.user.fetch();
+
+        if(Target.user.bot)
+        return interaction.reply({content: 'Нельзя выгнать бота...', ephemeral: true});
+        
+        if(Target.permissions.has("ADMINISTRATOR"))
+        return interaction.reply({content: `Пользователь ${Target.user} является администратором.`, ephemeral: true});
+
+        if(Target.roles.highest.position >= interaction.member.roles.highest.position)
+        return interaction.reply({content: 'Нельзя выгнать пользователя с более высокой ролью.', ephemeral: true});
+
+        if(Reason.length > 512)
+        return interaction.reply({content: "Причина не может быть больше 512 символов", ephemeral: true});
+        
+        if(Target.id === interaction.member.id) 
+        return interaction.reply({content: "Эй... Нельзя выгнать самого себя", ephemeral: true});
+
+        await Target.send({embeds: [new MessageEmbed().setColor("GOLD").setTitle("🦶 Вы были **выгнаны** с сервера").setTimestamp()
+        .setAuthor({name: Target.user.tag, iconURL: Target.user.avatarURL({dynamic: true, size: 512})})
+        .setDescription(`Вы были __выгнаны__ по причине: \`\`\`${Reason}\`\`\` \nСервер: **${interaction.guild.name}**`)
+        .setFooter({text: `ID: ${Target.user.id}`})]});
+
+        const Embed = new MessageEmbed().setTitle("Успешнное изгнание с сервера!")
+        .setColor("GOLD").setThumbnail(Target.user.avatarURL({ dynamic: true }))
+        .addFields(
+            { name: "**Пользователь**:", value: `${Target.user}` },
+            { name: "**Причина**:", value: `${Reason}` },
+            { name: "**Присоединился к нам**:", value: `<t:${parseInt(Target.joinedTimestamp / 1000)}:R>`, inline: true },
+            { name: "**Аккаунт создан**:", value: `<t:${parseInt(Target.user.createdTimestamp / 1000)}:R>`, inline: true }
+        )
+        .setFooter({text: `Пользователь: ${Target.user.tag} | ID: ${Target.user.id}`}).setTimestamp();
+
+        interaction.reply({ embeds: [Embed] }).then(() => setTimeout(()=> interaction.deleteReply(), 10000));
+        await Target.kick({ reason: Reason });
+
+        return;
+    }
+}
