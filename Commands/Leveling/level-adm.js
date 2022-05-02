88f3681705d5b5ae4fb89052c816a6ebcefaf7bb
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-05-02 06:31:40 +0300

    Перенос /xp в /level-adm

--- Commands/Leveling/level-adm.js
+++ Commands/Leveling/level-adm.js
@@ -1,5 +1,6 @@
 const { CommandInteraction, MessageEmbed } = require("discord.js");
 const LevelRewardDB = require("../../Structures/Schemas/Leveling/LevelRewardDB");
+const LevelDB = require("../../Structures/Schemas/Leveling/LevelingDB");
 
 module.exports = {
     name: 'level-adm',
@@ -15,6 +16,26 @@ module.exports = {
         },
         { name: 'remove-reward', description: 'Удалить награду за роль', type: "SUB_COMMAND",
             options: [{ name: 'role', description: 'Роль', type: 'ROLE', required: true }]
+        },
+        { name: 'manage', description: 'Управление опытом и уровнем пользователей', type: 'SUB_COMMAND',
+            options: [
+                { name: 'action', description: 'Действие, которое необходимо выполнить',
+                    type: 'STRING', required: true,
+                    choices: [
+                        { name: 'add', value: 'add' },
+                        { name: 'set', value: 'set' },
+                    ]
+                },
+                { name: 'type', description: 'Выберите, чтобы установить уровень или опыт',
+                    type: 'STRING', required: true,
+                    choices: [
+                        { name: 'уровень', value: 'level' },
+                        { name: 'опыт', value: 'xp' }
+                    ]
+                },
+                { name: 'user', description: 'Пользователь', type: 'USER', required: true, },
+                { name: 'amount', description: 'Количество', type: 'NUMBER', required: true }
+            ]
         }
     ],
     /**
@@ -55,6 +76,135 @@ module.exports = {
                 }
                 result.delete()
                 interaction.reply({ content: `Удалена награда за уровень`, ephemeral: true })
+            break;
+            case "manage" : {
+                const user = interaction.options.getUser("user");
+                let amountAdd = interaction.options.getNumber('amount');
+
+                switch(interaction.options.getString("type")) {
+                    case "level" : {
+                        switch(interaction.options.getString("action")) {
+                            case "add" : {
+                                const data = await LevelDB.findOne({ GuildID: interaction.guild.id, UserID: user.id });
+            
+                                let BeforeLevel;
+            
+                                if(!data) {
+                                    const newData = await LevelDB.create({
+                                        GuildID: interaction.guild.id, UserID: user.id, XP: 0, Level: 0, Cookies: 0
+                                    });
+                        
+                                    newData.save();
+                                } else {
+                                    BeforeLevel = data.Level;
+                                    data.Level += amountAdd;
+                                    data.save();
+                                }
+              
+                                const embed = new MessageEmbed()
+                                    .setTitle(`${user.username}'s level`)
+                                    .setFields(
+                                        { name: 'Было:', value: `\`${BeforeLevel}\``, inline: true },
+                                        { name: 'Стало:', value: `\`${data.Level}\``, inline: true },
+                                        { name: 'Добавлено:', value: `\`${amountAdd}\``, inline: true }
+                                    ).setColor('GREEN');
+            
+                                interaction.reply({ embeds: [embed] });
+                            }
+                            break;
+                            case "set" : {
+                                const data = await LevelDB.findOne({ GuildID: interaction.guild.id, UserID: user.id });
+            
+                                let BeforeLevel;
+            
+                                if(!data) {
+                                    const newData = await LevelDB.create({
+                                        GuildID: interaction.guild.id, UserID: user.id, XP: 0, Level: 0, Cookies: 0
+                                    });
+                    
+                                    newData.save();
+                                } else {
+                                    BeforeLevel = data.Level;
+                                    data.Level = amountAdd;
+                                    data.save();
+                                }
+                    
+                                const embed = new MessageEmbed()
+                                    .setTitle(`${user.username}'s level`)
+                                    .setFields(
+                                        { name: 'Было:', value: `\`${BeforeLevel}\``, inline: true },
+                                        { name: 'Стало:', value: `\`${data.Level}\``, inline: true }
+                                    ).setColor('GREEN');
+            
+                                interaction.reply({ embeds: [embed] });
+                            }
+                            break;
+                        }
+                    }
+                    break;
+
+                    case "xp" : {
+                        switch(interaction.options.getString("action")) {
+                            case "add" : {
+                                const data = await LevelDB.findOne({ GuildID: interaction.guild.id, UserID: user.id });
+              
+                                let BeforeXP;
+              
+                                if(!data) {
+                                    const newData = await LevelDB.create({
+                                        GuildID: interaction.guild.id, UserID: user.id, XP: 0, Level: 0, Cookies: 0
+                                    });
+              
+                                    newData.save();
+                                } else {
+                                    BeforeXP = data.XP;
+                                    data.XP += amountAdd;
+                                    data.save();
+                                }
+              
+                                const embed = new MessageEmbed()
+                                .setTitle(`${user.username}'s XP`)
+                                .setFields(
+                                    { name: 'Было:', value: `\`${BeforeXP}\``, inline: true },
+                                    { name: 'Стало:', value: `\`${data.XP}\``, inline: true },
+                                    { name: 'Добавлено:', value: `\`${amountAdd}\``, inline: true }
+                                ).setColor('GREEN');
+            
+                                interaction.reply({ embeds: [embed] });
+                            }
+                            break;
+                            case "set" : {
+                                const data = await LevelDB.findOne({ GuildID: interaction.guild.id, UserID: user.id });
+            
+                                let BeforeXP;
+            
+                                if(!data) {
+                                    const newData = await LevelDB.create({
+                                        GuildID: interaction.guild.id, UserID: user.id, XP: 0, Level: 0, Cookies: 0
+                                    });
+            
+                                    newData.save();
+                                } else {
+                                    BeforeXP = data.XP;
+                                    data.XP = amountAdd;
+                                    data.save();
+                                }
+              
+                                const embed = new MessageEmbed()
+                                .setTitle(`${user.username}'s XP`)
+                                .setFields(
+                                    { name: 'Было:', value: `\`${BeforeXP}\``, inline: true }, 
+                                    { name: 'Стало:', value: `\`${data.XP}\``, inline: true }
+                                ).setColor('GREEN');
+            
+                                interaction.reply({ embeds: [embed] });
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
             break;
         }
     }
