05fcb971f18cb29c7565873cc636a6d7bb64820a
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-05-02 06:27:13 +0300

    Правильный экспорт и новая функция

--- Utilites/LevelFucntions.js
+++ Utilites/LevelFucntions.js
@@ -1,28 +1,42 @@
 //Формулы взяты из JuniperBot
 
-
 //Расчёт опыта для получаения нового уровня
-exports.getLevelExp = async function getLevelExp(Level) {
-    return 5 * (Level * Level) + (50 * Level) + 100;
+exports.getLevelExp = getLevelExp;
+async function getLevelExp(Level) {
+    return (5 * (Level * Level) + (50 * Level) + 100);
     //return (Level * Level) * 100 + 100 // old func
 }
 
 //Расчёт всего опыта по уровню
-exports.getLevelTotalExp = async function getLevelTotalExp(Level) {
-    let exp = 0;
+exports.getLevelTotalExp = getLevelTotalExp;
+async function getLevelTotalExp(Level) {
+    let Exp = 0;
     for (var i = 0; i < Level; i++) {
-        exp + getLevelExp(i);
+        Exp + getLevelExp(i);
     }
 
-    return exp;
+    return Exp;
 }
 
 //
-exports.getRemainingExp = async function getRemainingExp(TotalExp) {
-    let remaining = TotalExp;
+exports.getLevelFromExp = getLevelFromExp;
+async function getLevelFromExp(Exp) {
+    let Level = 0;
+    while (Exp >= getLevelExp(Level)) {
+        Exp -= getLevelExp(Level);
+        Level++;
+    }
+    return Level;
+}
+
+
+//Всего опыта
+exports.getRemainingExp = getRemainingExp;
+async function getRemainingExp(TotalExp) {
+    let Remaining = TotalExp;
     for (var i = 0; i < getLevelFromExp(TotalExp); i++) {
-        remaining - getLevelExp(i);
+        Remaining - getLevelExp(i);
     }
 
-    return remaining;
+    return Remaining;
 }
