abac785776da9082e6dadaca53ce0d1f98ebd77e
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-05-13 19:06:05 +0300

    Добавляю работу с ветками

---
+++ Events/Thread/AutoCreate.js
@@ -0,0 +1,23 @@
+const { Message } = require("discord.js");
+const { CREATE_THREAD_TO_CHANNELS } = require("../../Structures/config.json");
+
+module.exports = {
+    name: "messageCreate",
+    /**
+     * @param {Message} message 
+     */
+    async execute(message) {
+        CREATE_THREAD_TO_CHANNELS.forEach(async (channel) => {
+            if(!message.guild.channels.cache.get(channel)) return;
+            if(message.channel.id != channel) return;
+
+            let Content = message.content;
+
+            for(var i = 0; i < message.content.length; ++i) {
+                Content = Content.replace("*", "").replace("_", "").replace("~", "").replace("`", "").replace("|", "")
+            }
+
+            await message.startThread({ name: `${Content.substring(0, 50)}...`});
+        });
+    }
+}
