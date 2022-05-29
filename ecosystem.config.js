00d70977f3b63127d5a7f522f2272264e1e92c90
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-05-30 01:51:14 +0300

    [ANY] Убираю файл экосистемы pm2

--- ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apps: [{
-        name: "CodeBot",
-        script: "Structures/index.js",
-        watch: true,
-        ignore_watch : ["node_modules", "Structures/Logs/"],
-    }]
-}
