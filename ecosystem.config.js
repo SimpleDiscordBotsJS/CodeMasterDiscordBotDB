b2dd5555d10601cc6d6933fbaf42756c479b5978
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2022-05-29 23:42:22 +0300

    [ANY] Добавляю конфиг для pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps: [{
+        name: "CodeBot",
+        script: "Structures/index.js",
+        watch: true,
+        ignore_watch : ["node_modules", "Structures/Logs"],
+    }]
+}
