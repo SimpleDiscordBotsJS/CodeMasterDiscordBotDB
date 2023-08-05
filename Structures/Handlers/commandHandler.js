afb0adb225a44ed5c5b0c49d80ebaa27b8049143
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2023-08-05 20:13:51 +0300

    [FEATURE] Попытка создать совместимость

--- Structures/Handlers/commandHandler.js
+++ Structures/Handlers/commandHandler.js
@@ -15,11 +15,15 @@ async function loadCommands(client) {
     for (const file of files) { 
         try { 
             const command = require(file);
-            client.commands.set(command.data.name, command);
-        
-            commandsArray.push(command.data.toJSON());
-        
-            table.addRow(command.data.name, "✔");
+            if (command.data) {
+                client.commands.set(command.data.name, command);
+                commandsArray.push(command.data.toJSON());
+                table.addRow(command.data.name, "✔");
+            } else {
+                client.commands.set(command.name, command);
+                commandsArray.push(command);
+                table.addRow(command.name, "✔");
+            }
         } catch (error) {
             table.addRow(file.split("/").pop().slice(0, -3), "✘");
             console.log(error)
