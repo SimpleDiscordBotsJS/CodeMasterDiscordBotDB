6ae98ae9ba30d6e372ed8a58f287dfa5f7b11604
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2023-08-07 14:37:11 +0300

    [REFACTOR] Обновил Приветствие/Прощание

--- Events/Member/guildMemberAdd.js
+++ Events/Member/guildMemberAdd.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, WebhookClient, GuildMember } = require("discord.js");
+const { EmbedBuilder, WebhookClient, GuildMember } = require("discord.js");
 
 module.exports = {
     name: "guildMemberAdd",
@@ -15,17 +15,22 @@ module.exports = {
 
         const Welcomer = new WebhookClient({ url: process.env.WEBHOOK_JOIN });
 
-        const Welcome = new MessageEmbed().setColor("AQUA")
-        .setAuthor({ name: user.tag, iconURL: user.avatarURL({dynamic: true, size: 512}) })
-        .setThumbnail(user.avatarURL({dynamic: true, size: 512}))
-        .setDescription(`Рады приветствовать ${member} на **${guild.name}**!\n\nАккаунт создан: <t:${parseInt(user.createdTimestamp / 1000)}:R>\nВсего на сервере: **${guild.memberCount}** человек`)
+        const Welcome = new EmbedBuilder().setColor("Aqua")
+        .setAuthor({ name: user.tag, iconURL: user.avatarURL({ dynamic: true, size: 512 }) })
+        .setThumbnail(user.avatarURL({ dynamic: true, size: 512 }))
+        .setDescription([
+            `Рады приветствовать ${member} на **${guild.name}**!`,
+            ``,
+            `Аккаунт создан: <t:${parseInt(user.createdTimestamp / 1000)}:R>`,
+            `Всего на сервере: **${guild.memberCount}** человек`
+        ].join("\n"))
         .addFields(
-            {name: `**Новости:**`, value: `<#962053378584752178>`, inline: true},
-            {name: `**Правила:**`, value: `<#962054528834863194>`, inline: true},
-            {name: `**Чат:**`, value: `<#962052402259837029>`, inline: true}
+            { name: `**Новости:**`, value: `<#962053378584752178>`, inline: true },
+            { name: `**Правила:**`, value: `<#962054528834863194>`, inline: true },
+            { name: `**Чат:**`, value: `<#962052402259837029>`, inline: true }
         )
-        .setFooter({text: `ID: ${user.id}`}).setTimestamp();
+        .setFooter({ text: `ID: ${user.id}` }).setTimestamp();
 
-        await Welcomer.send({ embeds: [Welcome]});
+        await Welcomer.send({ embeds: [Welcome] });
     }
 }
--- Events/Member/guildMemberRemove.js
+++ Events/Member/guildMemberRemove.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, WebhookClient, GuildMember } = require("discord.js");
+const { EmbedBuilder, WebhookClient, GuildMember } = require("discord.js");
 
 module.exports = {
     name: "guildMemberRemove",
@@ -11,14 +11,19 @@ module.exports = {
         //Проверка на наличие вебхука
         if(!process.env.WEBHOOK_EXIT) return;
 
-        const Logger = new WebhookClient({url: process.env.WEBHOOK_EXIT});
+        const Logger = new WebhookClient({ url: process.env.WEBHOOK_EXIT });
 
-        const Welcome = new MessageEmbed().setColor("AQUA")
-        .setAuthor({ name: user.tag, iconURL: user.avatarURL({dynamic: true, size: 512}) })
-        .setThumbnail(user.avatarURL({dynamic: true, size: 512}))
-        .setDescription(`${member} покинул нас, аминь.\n\nПрисоединился к нам: <t:${parseInt(member.joinedTimestamp / 1000)}:R>\nВсего на сервере: **${guild.memberCount}** человек`)
-        .setFooter({text: `ID: ${user.id}`}).setTimestamp();
+        const Welcome = new EmbedBuilder().setColor("Aqua")
+        .setAuthor({ name: user.tag, iconURL: user.avatarURL({ dynamic: true, size: 512 }) })
+        .setThumbnail(user.avatarURL({ dynamic: true, size: 512 }))
+        .setDescription([
+            `${member} покинул нас, аминь.`,
+            ``,
+            `Присоединился к нам: <t:${parseInt(member.joinedTimestamp / 1000)}:R>`,
+            `Всего на сервере: **${guild.memberCount}** человек`
+        ].join("\n"))
+        .setFooter({ text: `ID: ${user.id}` }).setTimestamp();
 
-        await Logger.send({ embeds: [Welcome]});
+        await Logger.send({ embeds: [Welcome] });
     }
 }
