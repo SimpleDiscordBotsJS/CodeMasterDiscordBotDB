827c7a9234c3cdc6012249e27ddbee47496a9047
LeonusDH <26181597+LeonusDH@users.noreply.github.com>
2023-10-26 01:26:26 +0300

    [REFACTOR] К чёрту старый фидер

--- Structures/Data/Schemas/FeedsDB.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-module.exports = model("FeedsData", new Schema({
-    GuildID: String,
-    FeedsData: {
-        PlayGround: { type: Number, default: null }
-    }
-})); 
